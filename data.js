34efdc796109b0ff1840271a5d4c17edfd40e1d6
m1onursal <74567342+m1onursal@users.noreply.github.com>
2025-12-23 23:55:21 +0300

    Create portalData with course details for semesters
    Added portalData object containing course information for multiple semesters.

---
+++ data.js
@@ -0,0 +1,58 @@
+const portalData = {
+    donem1: {
+        title: "Dönem 1",
+        courses: [
+            {
+                id: "anatomi",
+                name: "Anatomi",
+                code: "ECZ 103",
+                icon: "fa-skull",
+                weeks: [
+                    { week: 1, title: "Anatomiye Giriş", slide: "https://drive.google.com/file/d/1u29wadX4XX2xk552cL-Ulz4cpEUsv8gu/view?usp=share_link", quiz: "#" },
+                    { week: 2, title: "Kemikler", slide: "#", quiz: "#" },
+                    { week: 3, title: "Eklemler", slide: "#", quiz: "#" },
+                    { week: 4, title: "Kaslar", slide: "#", quiz: "#" },
+                    { week: 5, title: "Dolaşım Sistemi", slide: "#", quiz: "#" },
+                    { week: 6, title: "Solunum Sistemi", slide: "#", quiz: "#" },
+                    { week: 7, title: "Sindirim Sistemi", slide: "#", quiz: "#" },
+                    { week: 8, title: "Üriner Sistem", slide: "#", quiz: "#" },
+                    { week: 9, title: "Üreme Sistemi", slide: "#", quiz: "#" },
+                    { week: 10, title: "Sinir Sistemi", slide: "#", quiz: "#" },
+                    { week: 11, title: "Duyu Organları", slide: "#", quiz: "#" },
+                    { week: 12, title: "Endokrin Sistem", slide: "#", quiz: "#" }
+                ]
+            },
+            {
+                id: "bitki-biyo",
+                name: "Bitki Biyolojisi",
+                code: "ECZ 115",
+                icon: "fa-seedling",
+                weeks: [
+                    { week: 1, title: "1. Hafta", slide: "https://drive.google.com/file/d/1cUdIQbH51-bHWmt13dae6Vq-ffVrCx8M/view?usp=share_link", quiz: "https://gemini.google.com/share/a2a8701c289c" },
+                    { week: 2, title: "2. Hafta", slide: "https://drive.google.com/file/d/1AwbwwTL8tWsuerYUiDMr31hn0LLkAPbO/view?usp=share_link", quiz: "https://gemini.google.com/share/66c3faa1d8f2" },
+                    { week: 3, title: "3. Hafta", slide: "https://drive.google.com/file/d/1rNgeEXgNJzKMMhNItacuK1f9uBgj3rsM/view?usp=share_link", quiz: "https://gemini.google.com/share/70882a9f30c4" },
+                    { week: 4, title: "4. Hafta", slide: "https://drive.google.com/file/d/1_MSm8I36B5_k5fuxhKEnlYbSl_pj_cbG/view?usp=share_link", quiz: "https://gemini.google.com/share/79aa5fcbd88b" }
+                ]
+            },
+            { id: "tibbi-biyo", name: "Tıbbi Biyoloji", code: "ECZ 111", icon: "fa-dna", weeks: [] },
+            { id: "organik-kimya", name: "Organik Kimya I", code: "ECZ 117", icon: "fa-atom", weeks: [] },
+            { id: "fizik", name: "Fizik", code: "ECZ 105", icon: "fa-magnet", weeks: [] },
+            { id: "matematik", name: "Matematik", code: "ECZ 109", icon: "fa-calculator", weeks: [] },
+            { id: "biyoistatistik", name: "Biyoistatistik", code: "ECZ 113", icon: "fa-chart-line", weeks: [] },
+            { id: "genel-kimya", name: "Genel Kimya", code: "ECZ 107", icon: "fa-flask", weeks: [] },
+            { id: "ecz-giris", name: "Eczacılığa Giriş", code: "ECZ 101", icon: "fa-pills", weeks: [] },
+                    { id: "inkilap", name: "İnkılap Tarihi I", code: "AİİT 101", icon: "fa-landmark", weeks: [] },
+                    { id: "yabanci-dil", name: "Yabancı Dil I", code: "YD 101", icon: "fa-language", weeks: [] },
+                    { id: "turk-dili", name: "Türk Dili I", code: "TD 101", icon: "fa-pen-nib", weeks: [] }
+        ]
+    },
+    donem2: { title: "Dönem 2", courses: [] },
+    donem3: { title: "Dönem 3", courses: [] },
+    donem4: { title: "Dönem 4", courses: [] },
+    donem5: { title: "Dönem 5", courses: [] },
+    donem6: { title: "Dönem 6", courses: [] },
+    donem7: { title: "Dönem 7", courses: [] },
+    donem8: { title: "Dönem 8", courses: [] },
+    donem9: { title: "Dönem 9", courses: [] },
+    donem10: { title: "Dönem 10", courses: [] }
+};
